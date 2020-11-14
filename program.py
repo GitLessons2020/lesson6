064f72a65fc02bee067e5f67d6dabaa5e1b8f301
Danilkashtan <36625284+Danilkashtan@users.noreply.github.com>
2020-11-14 12:02:57 +0300

    update program.py and add docker compose

--- program.py
+++ program.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Base action'
+    return 'Hello from github actions'
 
 @app.route('/v2')
 def v2():
     return 'Second action'
+
+@app.route('/auto')
+def auto():
+    return 'Hello from auto update'
